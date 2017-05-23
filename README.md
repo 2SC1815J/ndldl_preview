7e984127090d965750a2505063dc0d4b39d8c53b
2SC1815J <2SC1815J@users.noreply.github.com>
2017-05-24 02:15:16 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ndldl_preview
+
+[国立国会図書館デジタルコレクション]向けユーザスクリプト
+
+国立国会図書館デジタルコレクションでの検索結果に表示されるサムネイルにマウスを重ねると、より大きい画像でプレビューできるようになります。
+
+[国立国会図書館デジタルコレクション]: http://dl.ndl.go.jp/
+[Firefox]: https://www.mozilla.org/firefox/
+[Greasemonkey]: https://addons.mozilla.org/firefox/addon/greasemonkey/
+
+## インストール方法
+
+1. このユーザスクリプトの利用には、[Greasemonkey]拡張機能がインストールされた[Firefox]が必要です。
+2. Greasemonkeyを初めてインストールした場合は、Firefoxを再起動してください。
+3. 以下のリンクをクリックすると、このユーザスクリプトのインストール確認が表示されます。
+    - [ndldl_preview.user.js](https://github.com/2SC1815J/ndldl_preview/raw/master/ndldl_preview.user.js)
+
+## 注意点
+
+国立国会図書館デジタルコレクションのサイトデザインが変更された場合、正しく動作しなくなる可能性があります。
---
+++ ndldl_preview.user.js
@@ -0,0 +1,38 @@
+﻿// ==UserScript==
+// @name        ndldl_preview
+// @description 国立国会図書館デジタルコレクション検索結果サムネイルのプレビュー
+// @author      2SC1815J
+// @released    2017-05-21
+// @license     MIT License
+// @namespace   https://github.com/2SC1815J
+// @homepageURL https://github.com/2SC1815J/ndldl_preview
+// @include     http://dl.ndl.go.jp/search/*
+// @require     https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
+// @require     https://code.jquery.com/jquery-1.12.4.min.js
+// @require     https://code.jquery.com/ui/1.9.2/jquery-ui.min.js
+// @grant       GM_addStyle
+// @version     0.2
+// ==/UserScript==
+
+(function ($) {
+    this.$ = this.jQuery = jQuery.noConflict(true);
+    GM_addStyle('.custom-tooltip { position: absolute; padding: 1px; opacity: 1; box-shadow: 0px 0px 5px #666666; }');
+    waitForKeyElements(
+        '#search-result-list',
+        resultListCallbackFunction
+    );
+    function resultListCallbackFunction() {
+        $('#search-result-list').tooltip({
+            items: '[src]',
+            content: function() {
+                return $(this).prop('outerHTML');
+            },
+            show: false,
+            position: {
+                my: 'left top',
+                at: 'right bottom'
+            },
+            tooltipClass: 'custom-tooltip'
+        });
+    }
+})(jQuery);
