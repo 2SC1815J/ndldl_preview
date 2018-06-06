0d084a63e9962b3a59cfedea2fd5574715218024
2SC1815J <2SC1815J@users.noreply.github.com>
2018-06-06 23:15:21 +0900

    add support for IIIF drag and drop link

--- ndldl_preview.user.js
+++ ndldl_preview.user.js
@@ -2,7 +2,7 @@
 // @name        ndldl_preview
 // @description 国立国会図書館デジタルコレクション検索結果サムネイルのプレビュー
 // @author      2SC1815J
-// @released    2017-05-21
+// @released    2018-06-06
 // @license     MIT License
 // @namespace   https://github.com/2SC1815J
 // @homepageURL https://github.com/2SC1815J/ndldl_preview
@@ -11,11 +11,11 @@
 // @require     https://code.jquery.com/jquery-1.12.4.min.js
 // @require     https://code.jquery.com/ui/1.9.2/jquery-ui.min.js
 // @grant       GM_addStyle
-// @version     0.2
+// @version     0.3
 // ==/UserScript==
 
-(function ($) {
-    this.$ = this.jQuery = jQuery.noConflict(true);
+(function($) {
+    //this.$ = this.jQuery = jQuery.noConflict(true);
     GM_addStyle('.custom-tooltip { position: absolute; padding: 1px; opacity: 1; box-shadow: 0px 0px 5px #666666; }');
     waitForKeyElements(
         '#search-result-list',
@@ -33,6 +33,21 @@
                 at: 'right bottom'
             },
             tooltipClass: 'custom-tooltip'
+        });
+        $('.item-icon-iiif').wrap(function() {
+            var manifestUrl;
+            var item_id = $(this).closest('td').find('span.result-item-id').text();
+            if (item_id) {
+                var match = item_id.match(/info:ndljp\/pid\/([0-9]+)/);
+                if (match) {
+                    manifestUrl = 'https://www.dl.ndl.go.jp/api/iiif/' + match[1] + '/manifest.json';
+                }
+            }
+            if (manifestUrl) {
+                return '<a href="' + manifestUrl + '" />';
+            } else {
+                return '';
+            }
         });
     }
 })(jQuery);
